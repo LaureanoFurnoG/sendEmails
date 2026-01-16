d87daf27c48d565a8c7edb6154fcc69fe4df46ac
Laureano Furno <148632238+voxaive@users.noreply.github.com>
2026-01-16 01:04:32 -0300

    fixed cors

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -2,7 +2,12 @@ import emailRouter from "../controllers/nodeMailer.js";
 
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
 
   if (req.method !== "POST") {
     return res.status(405).json({ msg: "Method not allowed" });
